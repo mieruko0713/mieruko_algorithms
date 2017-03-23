7acb3c1caf6219d41d49fcccba00d477880151c4
maruko0713 <wanzifun@gmail.com>
2017-03-23 18:11:56 +0800

    update 03.23

--- 1098.cpp
+++ 1098.cpp
@@ -2,15 +2,16 @@
 //  1098.cpp
 //  算法
 //
-//  Created by 王怡凡 on 17/2/26.
+//  Created by 王怡凡 on 2017/3/23.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
 #include<cstdio>
 #include<algorithm>
 using namespace std;
-const int maxn = 110;
-int n, tempOri[maxn],origin[maxn],changed[maxn];
+const int N=111;
+int origin[N],tempOri[N],changed[N];
+int n;
 
 bool isSame(int A[], int B[]) {
     int i;
@@ -26,9 +27,7 @@ void showArray(int A[]) {
     int i;
     for(i=1;i<=n;i++) {
         printf("%d",A[i]);
-        if(i<=n-1) {
-            printf(" ");
-        }
+        if(i<n) printf(" ");
     }
     printf("\n");
 }
@@ -37,16 +36,16 @@ bool insertSort() {
     bool flag = false;
     int i;
     for(i=1;i<n;i++) {
-        if(i!=1&&isSame(tempOri, changed)) {
+        if(isSame(tempOri,changed)) {
             flag = true;
         }
-        int temp = tempOri[i], j=i;
-        while(j>0&&tempOri[j-1] > temp) {
+        int temp = tempOri[i],j=i;
+        while(j>0&&tempOri[j-1]>temp) {
             tempOri[j] = tempOri[j-1];
             j--;
         }
         tempOri[j] = temp;
-        if(flag == true) {
+        if(flag==true) {
             return true;
         }
     }
@@ -59,10 +58,10 @@ void downAdjust(int low, int high) {
         if(j+1<=high&&tempOri[j+1]>tempOri[j]) {
             j=j+1;
         }
-        if(tempOri[j] > tempOri[i]) {
-            swap(tempOri[j],tempOri[i]);
+        if(tempOri[j]>tempOri[i]) {
+            swap(tempOri[i],tempOri[j]);
             i=j;
-            j=2*i;
+            j=i*2;
         } else {
             break;
         }
@@ -72,26 +71,25 @@ void downAdjust(int low, int high) {
 void heapSort() {
     bool flag = false;
     int i;
-    for(i=n/2;i>=1;i--){
-        downAdjust(i, n);
+    for(i=n/2;i>=1;i--) {
+        downAdjust(i,n);
     }
-    for(i=n;i>1;i++) {
-        if(i!=n&&isSame(tempOri,changed)) {
+    for(i=n;i>1;i--) {
+        if(isSame(tempOri,changed)) {
             flag = true;
         }
-        // printf("temoOri[i]: %d tempOri[1]: %d",tempOri[i],tempOri[1]);
         swap(tempOri[i],tempOri[1]);
-        downAdjust(1, i-1);
-        if(flag == true) {
+        downAdjust(1,i-1);
+        if(flag==true) {
             showArray(tempOri);
-            return ;
+            return;
         }
     }
 }
 
 int main() {
-    int i;
     scanf("%d",&n);
+    int i;
     for(i=1;i<=n;i++) {
         scanf("%d",&origin[i]);
         tempOri[i] = origin[i];
@@ -102,7 +100,7 @@ int main() {
     if(insertSort()) {
         printf("Insertion Sort\n");
         showArray(tempOri);
-    } else{
+    } else {
         printf("Heap Sort\n");
         for(i=1;i<=n;i++) {
             tempOri[i] = origin[i];
@@ -111,4 +109,3 @@ int main() {
     }
     return 0;
 }
-
