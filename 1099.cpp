8cbe456165af2342233b3274c53f5da00e0044fa
maruko0713 <wanzifun@gmail.com>
2017-03-23 18:31:42 +0800

    update 03.23

--- 1099.cpp
+++ 1099.cpp
@@ -2,33 +2,42 @@
 //  1099.cpp
 //  算法
 //
-//  Created by 王怡凡 on 17/2/24.
+//  Created by 王怡凡 on 2017/3/23.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
 #include<cstdio>
-#include<algorithm>
 #include<queue>
+#include<algorithm>
 using namespace std;
 
-const int maxn = 110;
-int in[maxn],num=0,n;
+const int maxn=110;
 struct node {
     int data;
     int lchild, rchild;
 }Node[maxn];
 
+int n, in[maxn],num=0;
+
+void inOrder(int root) {
+    if(root==-1) {
+        return;
+    }
+    inOrder(Node[root].lchild);
+    Node[root].data = in[num++];
+    inOrder(Node[root].rchild);
+}
+
 void BFS(int root) {
-    int now;
     queue<int> q;
     q.push(root);
     num = 0;
     while(!q.empty()) {
-        now = q.front();
+        int now = q.front();
         q.pop();
-        num++;
         printf("%d",Node[now].data);
-        if(num!=n) {
+        num++;
+        if(num<n) {
             printf(" ");
         }
         if(Node[now].lchild!=-1) {
@@ -40,28 +49,20 @@ void BFS(int root) {
     }
 }
 
-void inorder(int root) {
-    if(root==-1) {
-        return ;
-    }
-    inorder(Node[root].lchild);
-    Node[root].data = in[num++];
-    inorder(Node[root].rchild);
-}
-
 int main() {
-    int i,left,right;
+    int lchild, rchild;
     scanf("%d",&n);
-    for(i=0;i<n;i++) {
-        scanf("%d %d",&left,&right);
-        Node[i].lchild = left;
-        Node[i].rchild = right;
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d%d",&lchild,&rchild);
+        Node[i].lchild = lchild;
+        Node[i].rchild = rchild;
     }
     for(i=0;i<n;i++) {
         scanf("%d",&in[i]);
     }
     sort(in,in+n);
-    inorder(0);
+    inOrder(0);
     BFS(0);
+    return 0;
 }
-
