56a3998e40acb494ebe029bd62a53f3494ab5e9a
maruko0713 <wanzifun@gmail.com>
2017-08-20 11:22:36 +0800

    update 08.20

--- 1018.cpp
+++ 1018.cpp
@@ -2,43 +2,40 @@
 //  1018.cpp
 //  算法
 //
-//  Created by 王怡凡 on 2017/3/19.
+//  Created by 王怡凡 on 2017/8/20.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
-#include<cstdio>
+#include <stdio.h>
+#include<vector>
 #include<cstdlib>
 #include<algorithm>
-#include<vector>
-
 using namespace std;
-
-const int maxn=510;
+const int maxn = 510;
 const int INF = 1000000000;
-int G[maxn][maxn],d[maxn],w[maxn];
-vector<int> pre[maxn];
-vector<int> tempPath, path;
-bool vis[maxn] =  {false};
-int c,n,sp,m,minNeed=INF,minRemain=INF;
+vector<int> pre[maxn],tempPath,path;
+int G[maxn][maxn],d[maxn];
+bool vis[maxn] = {false};
+int cap,ed,n,m,c[maxn],minSend=INF,minBack=INF;
 
-void Dijkstra(int s) {
-    int i,j;
+void dijkstra(int s) {
     fill(d,d+maxn,INF);
     d[s] = 0;
+    int i,j,v;
     for(i=0;i<=n;i++) {
-        int MIN=INF,u=-1;
+        int u=-1,MIN=INF;
         for(j=0;j<=n;j++) {
-            if(d[j]<MIN&&vis[j]==false) {
-                u=j;
+            if(vis[j]==false&&d[j]<MIN ) {
+                u = j;
                 MIN = d[j];
             }
         }
-        if(u==-1) return;
+        if(u==-1) return ;
         vis[u] = true;
-        for(int v=0;v<=n;v++) {
-            if(G[u][v]!=INF&&vis[v]==false) {
-                if(G[u][v]+d[u]<d[v]){
-                    d[v] = G[u][v]+d[u];
+        for(v=0;v<=n;v++) {
+            if(vis[v]==false&&G[u][v]!=INF) {
+                if(d[u]+G[u][v]<d[v]) {
+                    d[v] = d[u] + G[u][v];
                     pre[v].clear();
                     pre[v].push_back(u);
                 } else if(d[u]+G[u][v]==d[v]) {
@@ -49,61 +46,65 @@ void Dijkstra(int s) {
     }
 }
 
-void DFS(int v) {
+void dfs(int v) {
     if(v==0) {
         tempPath.push_back(v);
-        int need=0,remain=0;
-        for(int i=tempPath.size()-1;i>=0;i--) {
+        int send=0,back=0,i,all=0,stand=cap/2,remain=0;
+        for(i=int(tempPath.size()-1);i>=0;i--) {
             int id = tempPath[i];
-            if(w[id]>0) {
-                remain+=w[id];
+            if(c[id]>0) {
+                remain += c[id];
+//                printf("remain:%d",remain);
             } else {
-                if(remain>abs(w[id])) {
-                    remain -= abs(w[id]);
+                if(remain>=abs(c[id])) {
+                    remain -= abs(c[id]);
                 } else {
-                    need += abs(w[id]) - remain;
-                    remain=0;
+                    send += abs(c[id]) - remain;
+                    remain = 0;
                 }
             }
         }
-        if(need<minNeed) {
-            minNeed = need;
-            minRemain = remain;
+        if(send<minSend) {
+            minSend = send;
             path = tempPath;
-        } else if(need==minNeed&&remain<minRemain) {
-            minRemain = remain;
+            minBack = remain;
+        } else if(send==minSend&&remain<minBack) {
+            minBack = remain;
             path = tempPath;
         }
         tempPath.pop_back();
         return ;
     }
     tempPath.push_back(v);
-    for(int i=0;i<pre[v].size();i++) {
-        DFS(pre[v][i]);
+    int i;
+    for(i=0;i<pre[v].size();i++) {
+        dfs(pre[v][i]);
     }
     tempPath.pop_back();
 }
 
+
 int main() {
-    scanf("%d %d %d %d",&c,&n,&sp,&m);
-    int i,u,v;
+    int u,v,i,dis;
+    scanf("%d%d%d%d",&cap,&n,&ed,&m);
+    for(i=0;i<n;i++) {
+        scanf("%d",&c[i+1]);
+        c[i+1] -= cap/2;
+    }
     fill(G[0],G[0]+maxn*maxn,INF);
-    for(i=1;i<=n;i++) {
-        scanf("%d",&w[i]);
-        w[i] -= c/2;
-    }
     for(i=0;i<m;i++) {
-        scanf("%d%d",&u,&v);
-        scanf("%d",&G[v][u]);
-        G[u][v] = G[v][u];
+        scanf("%d%d%d",&v,&u,&dis);
+        G[u][v] = G[v][u] = dis;
     }
-    Dijkstra(0);
-    DFS(sp);
-    printf("%d ",minNeed);
-    for(int i=path.size()-1;i>=0;i--) {
+    dijkstra(0);
+    dfs(ed);
+    printf("%d ",minSend);
+    for(i=int(path.size()-1);i>=0;i--) {
         printf("%d",path[i]);
-        if(i>0) printf("->");
+        if(i!=0) {
+            printf("->");
+        }
     }
-    printf(" %d",minRemain);
+    printf(" %d",minBack);
     return 0;
 }
