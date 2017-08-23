bd49dff778073926981beb44cc6723fd3fa540b1
maruko0713 <wanzifun@gmail.com>
2017-08-23 19:55:30 +0800

    update 08.23 1068

--- 1068.cpp
+++ 1068.cpp
@@ -2,61 +2,59 @@
 //  1068.cpp
 //  算法
 //
-//  Created by 王怡凡 on 2017/4/2.
+//  Created by 王怡凡 on 2017/8/23.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
-#include<cstdio>
+#include <stdio.h>
 #include<algorithm>
 using namespace std;
-
 const int maxn = 10010;
-const int maxv = 110;
-int w[maxn],dp[maxv]={0};
-bool choice[maxn][maxv],flag[maxn];
+int num[maxn],d[110]={0};
+bool flag[maxn] ={false};
+int choice[maxn][110]={0};
+int n,m;
+
 bool cmp(int a, int b) {
     return a>b;
 }
 
 int main() {
-    int i,n,m,v,k,num;
-    scanf("%d %d",&n,&m);
+    scanf("%d%d",&n,&m);
+    int i,j;
     for(i=1;i<=n;i++) {
-        scanf("%d",&w[i]);
+        scanf("%d",&num[i]);
     }
-    sort(w+1,w+n+1,cmp);
+    sort(num+1,num+n+1,cmp);
     for(i=1;i<=n;i++) {
-        for(v=m;v>=w[i];v--) {
-            if(dp[v]<=dp[v-w[i]]+w[i]) {
-                dp[v] = dp[v-w[i]] + w[i];
-                choice[i][v] = 1;
-            } else {
-                choice[i][v]=0;
+        for(j=m;j>=num[i];j--) {
+            if(d[j-num[i]]+num[i]>=d[j]) {
+                d[j] = d[j-num[i]] + num[i];
+                choice[i][j] = 1;
             }
         }
     }
-    if(dp[m]!=m) {
+    if(d[m]!=m) {
         printf("No Solution");
     } else {
-        k=n,num=0,v=m;
-        while(k>=0) {
-            if(choice[k][v]==true) {
-                flag[k] = true;
-                v -= w[k];
-                num++;
+        int k=n,v=m,cnt=0;
+        while(k>0) {
+            if(choice[k][v]==1) {
+                flag[k]=true;
+                v -= num[k];
+                cnt++;
+            } else {
+                flag[k]=false;
             }
-            else flag[k]=false;
             k--;
         }
-    }
-    for(i=n;i>=1;i--) {
-        if(flag[i]==true) {
-            printf("%d",w[i]);
-            num--;
-            if(num>0) {
-                printf(" ");
-            } else {
-                printf("\n");
+        for(int i=n;i>=1;i--) {
+            if(flag[i]==true) {
+                printf("%d",num[i]);
+                cnt--;
+                if(cnt>0) {
+                    printf(" ");
+                }
             }
         }
     }
