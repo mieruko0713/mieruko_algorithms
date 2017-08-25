efb7ced1fa66bf86a62e72ea31228314777f0b60
maruko0713 <wanzifun@gmail.com>
2017-08-25 16:07:16 +0800

    update 08.25 1087

--- 1087.cpp
+++ 1087.cpp
@@ -2,47 +2,42 @@
 //  1087.cpp
 //  算法
 //
-//  Created by 王怡凡 on 2017/3/20.
+//  Created by 王怡凡 on 2017/8/25.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
-#include<cstdio>
-#include<map>
-#include<string>
+#include <stdio.h>
 #include<algorithm>
+#include<map>
 #include<vector>
 #include<iostream>
 using namespace std;
-
+map<string,int> city;
+const int INF = 1000000000;
+map<int,string> num;
 const int maxn = 210;
-const int INF = 100000000;
-int G[maxn][maxn],w[maxn],d[maxn];
-int numPath = 0,maxW=-1,maxAvg=-1;
-int n,k;
-bool vis[maxn] = {false};
-vector<int> tempPath,path,pre[maxn];
-map<int,string> iToS;
-map<string, int> sToI;
+int happy[maxn],G[maxn][maxn],d[maxn],maxHappy=-1,maxAvg,numPath=0;
+int n,k,s=0;
+bool vis[maxn]={false};
+vector<int> path,tempPath,pre[maxn];
 
-void Dijkstra(int s) {
+void dijkstra(int s) {
     fill(d,d+maxn,INF);
     d[s] = 0;
-    int i;
-    for(i=0;i<n;i++) {
-        int u=-1,MIN=INF,j;
-        for(j=0;j<n;j++) {
-            if(vis[j]==false&&d[j]<MIN) {
+    for(int i=0;i<n;i++) {
+        int u=-1,MIN = INF;
+        for(int j=0;j<n;j++) {
+            if(d[j]<MIN&&vis[j]==false) {
                 u=j;
                 MIN = d[j];
             }
         }
         if(u==-1) return;
         vis[u] = true;
-        int v;
-        for(v=0;v<n;v++) {
+        for(int v=0;v<n;v++) {
             if(vis[v]==false&&G[u][v]!=INF) {
-                if(G[u][v]+d[u] < d[v]) {
-                    d[v] = G[u][v]+d[u];
+                if(G[u][v]+d[u]<d[v]) {
+                    d[v] = G[u][v] + d[u];
                     pre[v].clear();
                     pre[v].push_back(u);
                 } else if(G[u][v]+d[u]==d[v]) {
@@ -53,60 +48,72 @@ void Dijkstra(int s) {
     }
 }
 
-void DFS(int v) {
-    if(v==0) {
-        tempPath.push_back(v);
+void dfs(int v) {
+    if(v==s) {
         numPath++;
-        int tempW = 0;
-        for(int i=tempPath.size()-1;i>=0;i--) {
-            int id = tempPath[i];
-            tempW += w[id];
+        tempPath.push_back(v);
+        int sum=0;
+        double avg;
+        for(int i=int(tempPath.size())-2;i>=0;i--) {
+            sum += happy[tempPath[i]];
         }
-        
-        double tempAvg = 1.0*tempW/(tempPath.size()-1);
-//        printf("size:%d\n",tempPath.size()-1);
-//        printf("avg:%lf\n",tempAvg);
-        if(tempW>maxW) {
-            maxW = tempW;
-            maxAvg = tempAvg;
+        avg = sum*1.0 / (tempPath.size()-1);
+        if(sum>maxHappy) {
+            maxHappy = sum;
             path = tempPath;
-        } else if(tempW == maxW&&tempAvg>maxAvg) {
-            maxAvg = tempAvg;
-            path = tempPath;
+            maxAvg = avg;
+        } else if(sum==maxHappy) {
+            if(avg>maxAvg) {
+                path = tempPath;
+                maxAvg = avg;
+            }
         }
         tempPath.pop_back();
         return ;
     }
     tempPath.push_back(v);
     for(int i=0;i<pre[v].size();i++) {
-        DFS(pre[v][i]);
+        dfs(pre[v][i]);
     }
     tempPath.pop_back();
+    
 }
 
 int main() {
-    string start,c1,c2;
-    cin >> n >> k >>start;
-    iToS[0] = start;
+    cin>>n;
+    cin>>k;
+    cin>>num[0];
     int i;
+    string cityname;
+    city[num[0]] = 0;
     for(i=1;i<n;i++) {
-        cin >> c1 >> w[i];
-        sToI[c1] = i;
-        iToS[i] = c1;
+        cin >> cityname;
+        cin >> happy[i];
+        city[cityname] = i;
+        num[i] = cityname;
     }
     fill(G[0],G[0]+maxn*maxn,INF);
-    for(i=0;i<k;i++) {
-        cin >> c1 >> c2;
-        int id1 = sToI[c1],id2=sToI[c2];
-        cin >> G[id1][id2];
-        G[id2][id1] = G[id1][id2];
+    for(i=0;i<k;i++){
+        string name1,name2;
+        int cost;
+        cin >> name1;
+        cin >> name2;
+        cin >> cost;
+        int id1=city[name1],id2=city[name2];
+        G[id1][id2] = G[id2][id1] = cost;
     }
-    Dijkstra(0);
-    DFS(sToI["ROM"]);
-    printf("%d %d %d %d\n",numPath,d[sToI["ROM"]],maxW,maxAvg);
-    for(i=path.size()-1;i>=0;i--) {
-        cout << iToS[path[i]];
-        if(i!=0) cout << "->";
+    dijkstra(0);
+    dfs(city["ROM"]);
+//    printf("%d\n",d[city["ROM"]]);
+    int size =int(path.size());
+//    printf("%d\n",size);
+    int avg = maxHappy/(size-1);
+    printf("%d %d %d %d\n",numPath,d[city["ROM"]],maxHappy,avg);
+    for(i=size-1;i>=0;i--) {
+        cout << num[path[i]];
+        if(i!=0) {
+            printf("->");
+        }
     }
     return 0;
 }
