ca3274ceb1573b18c7cf2fbb76c0dd7b7fc73b78
maruko0713 <wanzifun@gmail.com>
2017-08-26 16:16:53 +0800

    update 08.26 1007

--- 1007.cpp
+++ 1007.cpp
@@ -2,44 +2,46 @@
 //  1007.cpp
 //  算法
 //
-//  Created by 王怡凡 on 2017/8/19.
+//  Created by 王怡凡 on 2017/8/26.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
 #include <stdio.h>
-
-int k;
-const int maxn = 10010;
+const int maxn = 100010;
+int n,all[maxn],d[maxn];
 
 int main() {
-    scanf("%d",&k);
-    bool flag = true;
-    int i,num[maxn],st,left=0,right=0,dp[maxn],maxnum;
-    for(i=0;i<k;i++) {
-        scanf("%d",&num[i]);
-        if(num[i]>=0) {
-            flag = false;
-        }
-        dp[i] = num[i];
-    }
-    st = 0;
-    maxnum = dp[0];
-    if(flag) {
-        printf("0 %d %d",num[0],num[k-1]);
-        return 0;
-    }
-    for(i=1;i<k;i++) {
-        if(dp[i-1]<0) {
-            st = i;
-            continue;
-        }
-        dp[i] = num[i] + dp[i-1];
-        if(dp[i]>maxnum) {
-            maxnum = dp[i];
-            left = st;
-            right = i;
+    int i,left=0,len=0,maxres,maxleft=0,maxlen=0;
+    scanf("%d",&n);
+    bool flag=false;
+    for(i=0;i<n;i++) {
+        scanf("%d",&all[i]);
+        if(all[i]>=0){
+            flag = true;
         }
     }
-    printf("%d %d %d",maxnum,num[left],num[right]);
+    if(!flag) {
+        printf("0 %d %d",all[0],all[n-1]);
+    } else {
+        d[0] = all[0];
+        maxres = d[0];
+        for(i=1;i<n;i++) {
+            if(d[i-1]<0) {
+                left = i;
+                len = 0;
+                d[i] = all[i];
+            } else {
+                d[i] = all[i]+d[i-1];
+                len++;
+            }
+            if(d[i]>maxres) {
+                maxres = d[i];
+                maxleft = left;
+                maxlen = len;
+            }
+        }
+        printf("%d %d %d",maxres,all[maxleft],all[maxleft+maxlen]);
+    }
+    
     return 0;
 }
