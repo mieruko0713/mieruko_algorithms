e2ac2c043cf15a40d6fbef647d9e62dc25c61414
maruko0713 <wanzifun@gmail.com>
2017-08-26 10:45:50 +0800

    update 08.26 1094

--- 1094.cpp
+++ 1094.cpp
@@ -2,45 +2,46 @@
 //  1094.cpp
 //  算法
 //
-//  Created by 王怡凡 on 2017/3/21.
+//  Created by 王怡凡 on 2017/8/26.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
-#include<cstdio>
+#include <stdio.h>
 #include<vector>
 using namespace std;
-
 const int maxn = 110;
-vector<int> Node[maxn];
-int Hash[maxn] = {0};
+vector<int> child[maxn];
+int layer[maxn]={0};
+int n,m,maxLayers=-1,maxId=1,maxNodes=-1,num=0;
 
-
-void DFS(int index, int level) {
-    Hash[level]++;
-    for(int i=0;i<Node[index].size();i++) {
-        DFS(Node[index][i],level+1);
+void dfs(int index,int lay) {
+    if(lay>maxLayers) {
+        maxLayers = lay;
+    }
+    layer[lay] += 1;
+    for(int i=0;i<child[index].size();i++) {
+        dfs(child[index][i],lay+1);
     }
 }
 
 int main() {
-    int n,m,parent,k,child;
     scanf("%d %d",&n,&m);
-    int i,j;
+    int i;
+    int k,id,v;
     for(i=0;i<m;i++) {
-        scanf("%d%d",&parent,&k);
-        for(j=0;j<k;j++) {
-            scanf("%d",&child);
-            Node[parent].push_back(child);
+        scanf("%d%d",&id,&k);
+        for(int j=0;j<k;j++) {
+            scanf("%d",&v);
+            child[id].push_back(v);
         }
     }
-    DFS(1,1);
-    int maxlev = -1,maxNode = -1;
-    for(i=1;i<=n;i++) {
-        if(Hash[i]>maxNode) {
-            maxlev = i;
-            maxNode = Hash[i];
+    dfs(1,1);
+    for(i=1;i<=maxLayers;i++) {
+        if(layer[i]>maxNodes) {
+            maxNodes = layer[i];
+            maxId = i;
         }
     }
-    printf("%d %d",maxNode,maxlev);
+    printf("%d %d",maxNodes,maxId);
     return 0;
 }
