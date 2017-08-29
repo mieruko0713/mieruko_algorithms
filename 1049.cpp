8e1f8409d529d233332db32a121539d7a96a8582
maruko0713 <wanzifun@gmail.com>
2017-08-29 16:46:06 +0800

    update 08.29 1049

---
+++ 1049.cpp
@@ -0,0 +1,30 @@
+//
+//  1049.cpp
+//  算法
+//
+//  Created by 王怡凡 on 2017/8/29.
+//  Copyright © 2017年 王怡凡. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    int n, a=1, ans=0;
+    int left, now, right;
+    scanf("%d",&n);
+    while(n/a!=0) {
+        left = n/(a*10);
+        now = n/a%10;
+        right = n%a;
+        if(now==0) {
+            ans += left*a;
+        } else if(now==1) {
+            ans += left*a+right+1;
+        } else {
+            ans += (left+1)*a;
+        }
+        a*=10;
+    }
+    printf("%d\n",ans);
+    return 0;
+}
