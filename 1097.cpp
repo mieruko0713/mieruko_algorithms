49a4e7d61c245822d102f000bdacdb4c8d2e82db
maruko0713 <wanzifun@gmail.com>
2017-08-29 19:58:11 +0800

    update 08.29 1097

--- 1097.cpp
+++ 1097.cpp
@@ -2,7 +2,7 @@
 //  1097.cpp
 //  算法
 //
-//  Created by 王怡凡 on 2017/8/26.
+//  Created by 王怡凡 on 2017/8/29.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
@@ -11,64 +11,49 @@
 #include<cstdlib>
 using namespace std;
 const int maxn = 100010;
-int st,n;
-int hashes[10010] = {false};
+int n,st;
 
 struct Node {
-    int address,data,next,order;
-}node[maxn*2];
+    int data,address,next,order;
+}node[maxn];
+
+int hashes[10010] = {0};
 
 bool cmp(Node a, Node b) {
     return a.order < b.order;
 }
 
-
-int main(){
+int main() {
     scanf("%d%d",&st,&n);
     int i,address;
-    for(i=0;i<maxn;i++) {
-        node[i].order = maxn;
-    }
     for(i=0;i<n;i++) {
         scanf("%d",&address);
-        
-        node[address].address = address;
-        scanf("%d %d",&node[address].data,&node[address].next);
+        node[address].address =address;
+        scanf("%d%d",&node[address].data,&node[address].next);
     }
-    int now = st,order=0;
+    for(i=0;i<maxn;i++) {
+        node[i].order=2*maxn;
+    }
+    int now =st,order=0,count1=0,count=0;
     while(now!=-1) {
-        node[now].order=order++;
+        node[now].order = order++;
+        int data = abs(node[now].data);
+        if(hashes[data]==0) {
+            hashes[data] = 1;
+            count1++;
+        } else {
+            node[now].order += maxn;
+        }
         now = node[now].next;
+        count++;
     }
     sort(node,node+maxn,cmp);
-    printf("%05d %d",node[0].address,node[0].data);
-    int count=0;
-    hashes[abs(node[0].data)] = true;
-    for(i=1;i<n;i++) {
-        int data = abs(node[i].data);
-        if(hashes[data]) {
-            node[maxn+count] = node[i];
-            count++;
-            if(i==n-1) {
-                printf(" -1\n");
-            }
-        } else {
-            printf(" %05d\n",node[i].address);
-            hashes[data] = true;
-            if(i!=n-1){
-               printf("%05d %d",node[i].address,node[i].data);
-            } else {
-                printf("%05d %d -1\n",node[i].address,node[i].data);
-            }
-        }
-    }
-    for(i=maxn;i<count+maxn;i++) {
-        if(i<maxn+count-1){
-            printf("%05d %d %05d\n",node[i].address,node[i].data,node[i+1].address);
-        } else {
+    for(i=0;i<count;i++) {
+        if(i==count1-1 || i==count-1) {
             printf("%05d %d -1\n",node[i].address,node[i].data);
+        }else {
+            printf("%05d %d %05d\n",node[i].address,node[i].data,node[i+1].address);
         }
-        
     }
     return 0;
 }
