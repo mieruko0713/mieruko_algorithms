0679452d918213cf54a69f4411deb54135d50296
maruko0713 <wanzifun@gmail.com>
2017-08-29 14:13:02 +0800

    update 08.29 1061

--- 1061.cpp
+++ 1061.cpp
@@ -2,46 +2,57 @@
 //  1061.cpp
 //  算法
 //
-//  Created by 王怡凡 on 17/3/6.
+//  Created by 王怡凡 on 2017/8/29.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
-#include<cstdio>
+#include <stdio.h>
 #include<cstring>
 using namespace std;
-
-int ans[3];
-char inp[4][70];
-char week[7][4] = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
+char weeks[7][4] = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
 
 int main() {
-    scanf("%s%s%s%s",inp[0],inp[1],inp[2],inp[3]);
-//    printf("%s\n",inp[1]);
-    int i,num=0,min1,min2;
-    min1 = strlen(inp[0])>strlen(inp[1])? strlen(inp[1]):strlen(inp[0]);
-    min2 = strlen(inp[2])>strlen(inp[3])? strlen(inp[3]):strlen(inp[2]);
-    for(i=0;i<min1;i++) {
-            if(inp[0][i]==inp[1][i]) {
-                if(num==0&&inp[0][i]>='A'&&inp[0][i]<='Z') {
-                    ans[0] = inp[0][i] - 'A';
+    char strs[4][62],week[4];
+    int len[4];
+    int i,min=0,h=0;
+    for(i=0;i<4;i++) {
+        scanf("%s",strs[i]);
+        len[i] = int(strlen(strs[i]));
+    }
+    int num=0;
+    i=0;
+    while(i<len[0]&&i<len[1]) {
+        if(strs[0][i]==strs[1][i]) {
+            if(num==0) {
+                char com = strs[1][i];
+                if(com>='A'&&com<='G') {
                     num++;
-                } else if(num==1) {
-                    if(inp[0][i]>='0'&&inp[0][i]<='9') {
-                        ans[1] = inp[0][i] - '0';
-                        break;
-                    } else if(inp[0][i]>='A'&&inp[0][i]<='N') {
-                        ans[1] = inp[0][i] - 'A'+10;;
-                        break;
-                    }
+                    strcpy(week,weeks[com-'A']);
+                }
+            } else if(num==1) {
+                char com = strs[1][i];
+                if(com>='0'&&com<='9') {
+                    h = com - '0';
+                    break;
+                } else if(com<='N'&&com>='A'){
+                    h = com - 'A' + 10;
+                    break;
                 }
             }
-    }
-    for(i=0;i<min2;i++) {
-        if(inp[2][i]==inp[3][i]&&(inp[2][i]>='a'&&inp[2][i]<='z'||inp[2][i]>='A'&&inp[2][i]<='Z')) {
-            ans[2] = i;
-            break;
         }
+        i++;
     }
-    printf("%s %02d:%02d",week[ans[0]],ans[1],ans[2]);
-    return 0;
+    i=0;
+    while(i<len[2]&&i<len[3]) {
+        if(strs[2][i]==strs[3][i]) {
+            char com = strs[2][i];
+            if((com>='a'&&com<='z')||(com>='A'&&com<='Z')) {
+                min = i;
+                break;
+            }
+            
+        }
+        i++;
+    }
+    printf("%s %02d:%02d",week,h,min);
 }
