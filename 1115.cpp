93f414a02cc8c0116c9ebdafd02f532fec722692
maruko0713 <wanzifun@gmail.com>
2017-08-30 17:27:51 +0800

    update 08.30 1115

--- 1115.cpp
+++ 1115.cpp
@@ -1,55 +1,57 @@
 //
 //  1115.cpp
 //  算法
-// 
-//  Created by 王怡凡 on 17/2/23.
+//
+//  Created by 王怡凡 on 2017/8/30.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
-#include<cstdio>
-#include<algorithm>
-using namespace std;
-
+#include <stdio.h>
+int n;
 const int maxn = 1010;
-int n, num[maxn]={0}, maxdepth=-1;
-struct node {
+int layer[maxn] = {0},maxlayer;
+
+struct Node {
     int data;
-    node *left, *right;
+    Node *lchild,*rchild;
 };
 
-void DFS(node *root, int depth) {
+void insert(Node* &root, int data) {
     if(root==NULL) {
-        maxdepth = max(depth, maxdepth);
-        return;
-    }
-    num[depth]++;
-    DFS(root->left, depth+1);
-    DFS(root->right, depth+1);
-}
-
-void insert(node* &root, int data) {
-    if(root==NULL) {
-        root = new node;
-        root->left = NULL;
-        root->right = NULL;
+        root = new Node;
         root->data = data;
+        root->lchild = NULL;
+        root->rchild = NULL;
         return ;
     }
     if(data<=root->data) {
-        insert(root->left, data);
-        insert(root->right, data);
+        insert(root->lchild,data);
+    } else {
+        insert(root->rchild,data);
     }
 }
 
+void dfs(Node *root, int num) {
+    if(root==NULL) {
+        return ;
+    }
+    if(num>maxlayer) {
+        maxlayer=num;
+    }
+    layer[num++] += 1;
+    dfs(root->lchild,num);
+    dfs(root->rchild,num);
+}
+
 int main() {
-    int n,i,data;
-    node *root = NULL;
     scanf("%d",&n);
+    int i,data;
+    Node *root;
     for(i=0;i<n;i++) {
         scanf("%d",&data);
-        insert(root, data);
+        insert(root,data);
     }
-    DFS(root,0);
-    printf("%d + %d = %d", num[maxdepth-1], num[maxdepth-2],num[maxdepth-1]+num[maxdepth-2]);
+    dfs(root,0);
+    printf("%d + %d = %d",layer[maxlayer],layer[maxlayer-1],layer[maxlayer]+layer[maxlayer-1]);
     return 0;
 }
