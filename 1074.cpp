de8acf3bc9d7c13a6edb9b0e0d335a57a4be8d2a
maruko0713 <wanzifun@gmail.com>
2017-08-31 09:34:29 +0800

    update 08.31 1074

--- 1074.cpp
+++ 1074.cpp
@@ -2,7 +2,7 @@
 //  1074.cpp
 //  算法
 //
-//  Created by 王怡凡 on 2017/8/23.
+//  Created by 王怡凡 on 2017/8/30.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
@@ -12,58 +12,51 @@ using namespace std;
 const int maxn = 100010;
 int st,n,k;
 
-struct Node {
+struct Node{
     int address,data,next,order;
 }node[maxn];
 
 bool cmp(Node a, Node b) {
-    return a.order<b.order;
+    return a.order < b.order;
 }
 
 int main() {
-    scanf("%d%d%d",&st,&n,&k);
-    int i,j,address;
+    scanf("%d %d %d",&st,&n,&k);
+    int i,address;
     for(i=0;i<maxn;i++) {
         node[i].order = maxn;
     }
-
-    for(i=0;i<n;i++) {
+    for(i=0;i<n;i++){
         scanf("%d",&address);
         node[address].address = address;
         scanf("%d %d",&node[address].data,&node[address].next);
-//        printf("%d\n",node[address].data);
     }
-    int p = st,count=0;
-    while(p!=-1) {
-        count++;
-        node[p].order = count;
-        p = node[p].next;
+    int now = st,order=0;
+    while(now!=-1) {
+        node[now].order = order++;
+        now = node[now].next;
     }
-    
+    n = order;
     sort(node,node+maxn,cmp);
-//    printf("node[0]:%d\n",node[0].address);
+    int j;
     for(i=0;i<n/k;i++) {
         for(j=(i+1)*k-1;j>i*k;j--) {
-            printf("%05d %d %05d\n",node[j].address,node[j].data,node[j-1].address);
+                printf("%05d %d %05d\n",node[j].address,node[j].data,node[j-1].address);
         }
-        printf("%05d %d ",node[j].address,node[j].data);
-        if(i<n/k-1) {
-            printf("%05d\n",node[(i+2)*k-1].address);
-        } else if(i==n/k-1) {
-            if(n%k==0) {
-                printf("-1\n");
-            } else {
-                printf("%05d\n",node[n/k*k].address);
-            for(int i=n/k*k;i<n;i++) {
-                printf("%05d %d ",node[i].address,node[i].data);
-                if(i<n-1) {
-                    printf("%05d\n",node[i+1].address);
-                } else {
-                    printf("-1\n");
-                }
-            }
-            }
+        if(i!=n/k-1) {
+           printf("%05d %d %05d\n",node[j].address,node[j].data,node[(i+2)*k-1].address);
+        } else {
+           printf("%05d %d ",node[j].address,node[j].data);
         }
+    }
+    if(n%k==0) {
+        printf("-1\n");
+    } else {
+        printf("%05d\n",node[(n/k)*k].address);
+        for(j=(n/k)*k;j<n-1;j++) {
+            printf("%05d %d %05d\n",node[j].address,node[j].data,node[j].next);
+        }
+        printf("%05d %d -1\n",node[j].address,node[j].data);
     }
     return 0;
 }
