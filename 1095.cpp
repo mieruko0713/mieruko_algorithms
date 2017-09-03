86718f0f55b1151028020f6e0b9e14e5c586bb3e
maruko0713 <wanzifun@gmail.com>
2017-09-03 12:57:22 +0800

    update 09.03 1075

--- 1095.cpp
+++ 1095.cpp
@@ -2,83 +2,100 @@
 //  1095.cpp
 //  算法
 //
-//  Created by 王怡凡 on 2017/3/21.
+//  Created by 王怡凡 on 2017/9/3.
 //  Copyright © 2017年 王怡凡. All rights reserved.
 //
 
-#include<cstdio>
+#include <stdio.h>
 #include<algorithm>
-#include<map>
-#include<string>
 #include<cstring>
+#include<map>
+#include<vector>
+#include<string>
 using namespace std;
-
+int n,k,maxTime=-1;
 const int maxn = 10010;
-struct car {
+vector<string> ans;
+map<string, int> parkTime;
+
+struct Car {
     char id[8];
     int time;
-    char status[4];
-}all[maxn],valid[maxn];
-int num=0;
-map<string,int> parkTime;
+    bool status;
+}cars[maxn],valid[maxn];
+
+
+bool cmp(Car a, Car b) {
+    if(strcmp(a.id,b.id)) {
+        return strcmp(a.id,b.id)<0;
+    } else {
+        return a.time<b.time;
+    }
+}
+
+bool cmpByTime(Car a, Car b) {
+    return a.time < b.time;
+}
 
 int timeToInt(int hh, int mm, int ss) {
     return hh*3600+mm*60+ss;
 }
 
-bool cmpByIdAndTime(car a , car b) {
-    if(strcmp(a.id,b.id)) {
-        return strcmp(a.id,b.id)<0;
-    }
-    return a.time<b.time;
-}
-
-bool cmpByTime(car a, car b) {
-    return a.time<b.time;
-}
-
 int main() {
-    int n,k,hh,mm,ss;
-    scanf("%d%d",&n,&k);
-    int i;
+    int hh,mm,ss;
+    char status[4];
+    scanf("%d %d",&n,&k);
+    int i=0;
     for(i=0;i<n;i++) {
-        scanf("%s %d:%d:%d %s",all[i].id,&hh,&mm,&ss,all[i].status);
-        all[i].time = timeToInt(hh,mm,ss);
-    }
-    sort(all,all+n,cmpByIdAndTime);
-    int maxTime = -1;
-    for(i=0;i<n-1;i++) {
-        if(!strcmp(all[i].id,all[i+1].id)&&!strcmp(all[i].status,"in")
-           &&!strcmp(all[i+1].status,"out")) {
-            valid[num++] = all[i];
-            valid[num++] = all[i+1];
-            int inTime = all[i+1].time - all[i].time;
-            if(parkTime.count(all[i].id)==0) {
-                parkTime[all[i].id] = 0;
-            }
-            parkTime[all[i].id] += inTime;
-            maxTime = max(maxTime, parkTime[all[i].id]);
+        scanf("%s %d:%d:%d %s",cars[i].id,&hh,&mm,&ss,status);
+        cars[i].time = timeToInt(hh,mm,ss);
+        if(strcmp(status,"in")==0) {
+            cars[i].status = true;
+        } else {
+            cars[i].status = false;
         }
     }
-    sort(valid,valid+num,cmpByTime);
-    int now=0,numCar=0;
+    sort(cars,cars+n,cmp);
+    int num1=0;
+    for(i=0;i<n-1;i++) {
+        if(!strcmp(cars[i].id,cars[i+1].id)&&cars[i].status==true&&cars[i+1].status==false) {
+            valid[num1++]=cars[i];
+            valid[num1++]=cars[i+1];
+            int inTime = cars[i+1].time - cars[i].time;
+            string id = cars[i].id;
+//            if(parkTime.count(id)==0) {
+//                parkTime[id] = 0;
+//            }
+            parkTime[id] += inTime;
+            if(maxTime<parkTime[id]) {
+                maxTime = parkTime[id];
+            }
+        }
+    }
+    sort(valid,valid+num1,cmpByTime);
+    int num=0,j=0;
     for(i=0;i<k;i++) {
         scanf("%d:%d:%d",&hh,&mm,&ss);
         int time = timeToInt(hh,mm,ss);
-        while(now<num&&valid[now].time<=time) {
-            if(!strcmp(valid[now].status,"in")) numCar++;
-            else numCar--;
-            now++;
+        for(;j<num1;j++) {
+            if(valid[j].time>time) {
+                break;
+            } else {
+                if(valid[j].status) {
+                    num++;
+                } else {
+                    num--;
+                }
+            }
         }
-        printf("%d\n",numCar);
+        printf("%d\n",num);
     }
-    map<string,int>::iterator it;
-    for(it=parkTime.begin();it!=parkTime.end();it++) {
-        if(it->second==maxTime) {
-            printf("%s ", it->first.c_str());
+        map<string,int>::iterator it;
+        for(it=parkTime.begin();it!=parkTime.end();it++) {
+            if(it->second==maxTime) {
+                printf("%s ",it->first.c_str());
+            }
         }
-    }
-    printf("%02d:%02d:%02d",maxTime/3600,maxTime%3600/60,maxTime%60);
+        printf("%02d:%02d:%02d",maxTime/3600,maxTime%3600/60,maxTime%60);
     return 0;
 }
-
